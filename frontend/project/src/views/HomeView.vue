dc47dc59e38e1d167ddc8b80ac3043fa31b8af83
JuanLuther <juanlutherhernando@gmail.com>
2022-04-26 20:26:33 +0800

    set-up complete

--- frontend/project/src/views/HomeView.vue
+++ frontend/project/src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="my-5 p-4">
+    <p>A&E Architectural Firm</p>
+    <p>Images</p>
+    <div class="flex justify-center gap-5">
+      <button class="btn-blue">CLick me</button>
+      <button class="btn-blue">CLick me</button>
+    </div>
   </main>
 </template>
